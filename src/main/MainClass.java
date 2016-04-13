be2cabde8dfc3d55a0a2f72fe435dff1aa53ff4f
Marco Greselin <marcogreselin@me.com>
2016-04-13 19:43:28 +0100

    Changed name in comments

--- src/main/MainClass.java
+++ src/main/MainClass.java
@@ -4,7 +4,7 @@ import java.awt.EventQueue;
 import UI.LoginPage;
 
 /**
- * 'Sanem' is a Patient Registry developed as part of 
+ * 'Sanus' is a Patient Registry developed as part of 
  * UCL course COMPGC01 as a main Coursework.
  * It features a number of implementations including an
  * SQL database for the management of the users and the patients'
